0970d27de8cd951808e3738cca27c5a08590a4a9
Leo Liang <festony@gmail.com>
2021-06-14 12:22:20 +1000

    Init commit: plain springboot project + camel dependencies

---
+++ src/main/kotlin/com/festony/imagecamel/ImageCamelApplication.kt
@@ -0,0 +1,11 @@
+package com.festony.imagecamel
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ImageCamelApplication
+
+fun main(args: Array<String>) {
+	runApplication<ImageCamelApplication>(*args)
+}
